236db2da25511ec21c5aefc45856d20b3221aa2e
Anish-252 <97762337+Anish-252@users.noreply.github.com>
2023-09-04 22:31:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontSize:{
+        'l':'1rem'
+      },
+      colors:{
+        'lblue':'#1877f2',
+        'lgreen':'#50bd22',
+        'dgreen':'#49a322'
+      }
+    },
+  },
+  plugins: [],
+}
